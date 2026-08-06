3e4a07763c54c1377b84efcfc90cb9b8ad6b41ee
arasif10 <atiqurrahmanasif281@gmail.com>
2026-08-06 21:18:27 +0600

    Fix buildSrc dependencies using explicit string coordinates

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,9 +2,15 @@ plugins {
     `kotlin-dsl`
 }
 
+repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+}
+
 dependencies {
-    implementation(libs.gradle.agp)
-    implementation(libs.gradle.kotlin)
-    implementation(libs.gradle.kotlin.serialization)
-    implementation(libs.gradle.kotlinter)
+    implementation("com.android.tools.build:gradle:8.2.1")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+    implementation("org.jetbrains.kotlin:kotlin-serialization:1.8.22")
+    implementation("org.jmailen.gradle:kotlinter-gradle:3.15.0")
 }
