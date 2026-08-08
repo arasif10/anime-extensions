42322cef99c326741a0777991f28219cbaa6bdf7
arasif10 <atiqurrahmanasif281@gmail.com>
2026-08-09 01:39:11 +0600

    fix: import ordering in ToonWorld4All to satisfy lint

--- src/all/toonworld4all/src/eu/kanade/tachiyomi/animeextension/all/toonworld4all/ToonWorld4All.kt
+++ src/all/toonworld4all/src/eu/kanade/tachiyomi/animeextension/all/toonworld4all/ToonWorld4All.kt
@@ -10,8 +10,8 @@ import eu.kanade.tachiyomi.network.GET
 import okhttp3.Headers
 import okhttp3.Request
 import okhttp3.Response
-import org.jsoup.Jsoup
 import org.json.JSONObject
+import org.jsoup.Jsoup
 
 class ToonWorld4All : AnimeHttpSource() {
 
